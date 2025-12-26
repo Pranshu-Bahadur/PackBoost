5184f42653358c94b2d7e91e7587c1348b282005
jmnum <jmbastide@gmail.com>
2025-12-26 16:19:01 +0100

    Modify add_idx calculation in core.py
    Updated index calculation for add_idx to use (1-x) instead of 1+x.

--- packboost/core.py
+++ packboost/core.py
@@ -848,8 +848,8 @@ class PackBoost(BaseEstimator, RegressorMixin):
                     else:
                         raise AssertionError("L_new must be uint8 or uint16")
 
-                add_idx = (2 * lo + 1 + x).to(torch.long)
-                add_idx=torch.clamp(add_idx, max=max_idx)
+                add_idx = (2 * lo + (1-x) ).to(torch.long) ### updated 
+                ###add_idx=torch.clamp(add_idx, max=max_idx)
                 P.add_(V[tree_set, f].gather(0, add_idx))
 
         return P, L_new
